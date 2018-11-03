21f71818e96138b4cc006f6a8b963e15ed9c1759
haliliya <haliliya@post.bgu.ac.il>
2018-11-03 15:19:26 +0200

    data structures added

--- src/Parse.java
+++ src/Parse.java
@@ -1,4 +1,8 @@
+import java.util.HashSet;
+
 public class Parse {
+   private HashSet <String> capitalLetters = new HashSet<String>();
+   private HashSet <String> terms = new HashSet<String>();
 
    public void removeStopWords (String docText){
        
@@ -9,6 +13,10 @@ public class Parse {
 
    public void numParse (String text){
 
+   }
+
+   public void lettersCase (String token){
+       
    }
 
 
