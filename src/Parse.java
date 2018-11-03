e804c30f393371cc4cb046458ffc1135cd528639
shaharar <shaharar@post.bgu.ac.il>
2018-11-03 15:22:30 +0200

    numParse

--- src/Parse.java
+++ src/Parse.java
@@ -7,9 +7,14 @@ public class Parse {
 
    }
 
-   public void numParse (String text){
+   public void numParse (String text, int nextIdx){
+      int num = Integer.parseInt(text);
+      //num is less than 1000
+      if(num < 1000){
+         //num has a fraction after it
+         
 
+      }
    }
-
 
 }
