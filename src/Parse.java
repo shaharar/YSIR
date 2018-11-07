d0202cfd952108cc0432e0b212e6185a58b54985
shaharar <shaharar@post.bgu.ac.il>
2018-11-07 16:19:29 +0200

    negative number

--- src/Parse.java
+++ src/Parse.java
@@ -105,48 +105,53 @@ public class Parse {
       if(token.startsWith("-")){
          num = Double.parseDouble(token.substring(1)) * (-1);
       }
-      else
-         num = Double.parseDouble(token);
-      //num is less than 1,000
-      if (num < 1000) {
-         //num has a fraction after it - like '34 2/3'
-         if (tokens[nextIdx].contains("/")) {
-            terms.add(token + tokens[nextIdx]);
-         }
-         //Thousand after num - like '50 Thousand'
-         else if (tokens[nextIdx].equals("Thousand")) {
-            terms.add(token + "K");
-         }
-         //Million after num - like '50 Million'
-         else if (tokens[nextIdx].equals("Million")) {
-            terms.add(token + "M");
-         }
-         //Billion after num - like '50 Billion'
-         else if (tokens[nextIdx].equals("Billion")) {
-            terms.add(token + "B");
-         }
-         //Trillion after num - like '50 Trillion'
-         else if (tokens[nextIdx].equals("Trillion")) {
-            terms.add((num * 1000) + "B");
-         }
-         //just number - like '123'
-         else {
-            terms.add(token);
-         }
-      }
-      //num is up to 1,000
-      else {
-         //num between 1,000 to 999,000
-         if (num < 1000000) {
-            terms.add((num / 1000) + "K");
-         }
-         //num between 1,000,000 to 999,000,000
-         else if (num < 1000000000) {
-            terms.add((num / 1000000) + "M");
-         }
-         //num up to 1,000,000,000
-         else {
-            terms.add((num / 1000000000) + "B");
+      else{
+         try{
+            num = Double.parseDouble(token);
+            //num is less than 1,000
+            if (num < 1000) {
+               //num has a fraction after it - like '34 2/3'
+               if (tokens[nextIdx].contains("/")) {
+                  terms.add(token + tokens[nextIdx]);
+               }
+               //Thousand after num - like '50 Thousand'
+               else if (tokens[nextIdx].equals("Thousand")) {
+                  terms.add(token + "K");
+               }
+               //Million after num - like '50 Million'
+               else if (tokens[nextIdx].equals("Million")) {
+                  terms.add(token + "M");
+               }
+               //Billion after num - like '50 Billion'
+               else if (tokens[nextIdx].equals("Billion")) {
+                  terms.add(token + "B");
+               }
+               //Trillion after num - like '50 Trillion'
+               else if (tokens[nextIdx].equals("Trillion")) {
+                  terms.add((num * 1000) + "B");
+               }
+               //just number - like '123'
+               else {
+                  terms.add(token);
+               }
+            }
+            //num is up to 1,000
+            else {
+               //num between 1,000 to 999,000
+               if (num < 1000000) {
+                  terms.add((num / 1000) + "K");
+               }
+               //num between 1,000,000 to 999,000,000
+               else if (num < 1000000000) {
+                  terms.add((num / 1000000) + "M");
+               }
+               //num up to 1,000,000,000
+               else {
+                  terms.add((num / 1000000000) + "B");
+               }
+            }
+         } catch (NumberFormatException e) {
+            e.printStackTrace();
          }
       }
    }
