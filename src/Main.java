edc8136dcbcedbb8f9006159d7c2dfd20f7831b7
haliliya <@sadfas>
2018-11-30 11:54:17 +0200

    delete string builders + fw = null

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ import java.io.IOException;
 public class Main {
 
     public static void main (String [] args) throws IOException {
-        ReadFile rf = new ReadFile(false, "d:\\documents\\users\\shaharar\\Downloads\\test");
+        ReadFile rf = new ReadFile(false, "d:\\documents\\users\\haliliya\\Downloads\\test");
         try {
-            rf.getFilesFromDir("d:\\documents\\users\\shaharar\\Downloads\\corpus\\corpus");
+            rf.getFilesFromDir("d:\\documents\\users\\haliliya\\Downloads\\corpus\\corpus");
         } catch (IOException e) {
             e.printStackTrace();
         }
