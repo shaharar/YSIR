af421b5443cde680c7dd8336dfff249c5e645f8d
haliliya <vgbch>
2018-12-20 14:48:34 +0200

    Ranker

--- src/Model/Ranker.java
+++ src/Model/Ranker.java
@@ -1,32 +1,78 @@
 package Model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.TreeMap;
+import javafx.util.Pair;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class Ranker {
-    private double avdl;
-    private int N;
     private double k;
     private double b;
-    private TreeMap <String, Double> docsRanks;
+//    private TreeMap <String, Double> docsRanks;
+    private PriorityQueue <Pair <String, Double>> docsRanks = new PriorityQueue<>(new Comparator<Pair<String, Double>>() {
+    @Override
+    public int compare(Pair<String, Double> p1, Pair<String, Double> p2) {
+        return (p1.getValue().compareTo(p2.getValue()));
+    }
+});
 
     public Ranker (){
-        avdl = 0;
-        N = 0;
-        docsRanks = new TreeMap<>();
+//        docsRanks = new TreeMap<>();
     }
-    public void rank(HashMap<String, HashMap<String, Integer>> docsResults, HashMap<String, Integer> queryTerms, HashMap<String, ArrayList<Integer>> dictionary, String queryId, String queryDescription) {
+    public void rank(HashMap<String, HashMap<String, Integer>> docsResults, HashMap<String, Integer> queryTerms, HashMap<String, ArrayList<Integer>> dictionary,HashMap <String, Integer> docsInfo, String queryId, String queryDescription) {
+        int totalDocsLengths = 0, N;
+        double avdl;
+        for (Integer docLength: docsInfo.values()) {
+            totalDocsLengths += docLength;
+        }
+        N = docsInfo.size();
+        avdl = totalDocsLengths / N;
         for (String docId: docsResults.keySet()) {
-            double rank;
-            int docTf, queryTf, df, docLength;
+            double rank = 0;
+            int docTf = 0, queryTf = 0, df = 0, docLength = 0;
             for (String term: queryTerms.keySet()) {
                 docTf = docsResults.get(docId).get(term);
                 queryTf = queryTerms.get(term);
                 df = dictionary.get(term).get(1);
+                docLength = docsInfo.get(docId);
             }
+            rank += queryTf * (((k + 1) * docTf) / (docTf + k * (1 - b + b * (docLength / avdl)))) * Math.log(N / df);
+            docsRanks.add(new Pair<>(docId, rank));
+//            docsRanks.put(docId, rank);
         }
+        while (docsRanks.size() > 50){
+            docsRanks.poll();
+        }
+
+        //        Map <String, Double> results = new TreeMap<>();
+//        results.putAll(getTop50Docs(docsRanks));
+
     }
 
+    public PriorityQueue<Pair<String, Double>> getDocsRanks() {
+        return docsRanks;
+    }
+
+
+
+    //    private Map<String, Double> getTop50Docs(TreeMap<String, Double> docsRanks) {
+//        Comparator <String> valueComparator = new Comparator<String>() {
+//            @Override
+//            public int compare(String s1, String s2) {
+//                return docsRanks.get(s1).compareTo(docsRanks.get(s2));
+//            }
+//        };
+//        TreeMap <String, Double> sortedResultsByValues = new TreeMap<>(valueComparator);
+//        sortedResultsByValues.putAll(docsRanks);
+//        return sortedResultsByValues;
+//    }
+//
+//    public TreeMap<String, Double> getDocsRanks() {
+//        return docsRanks;
+//    }
+
+    public void writeResultsToDisk(String queryId){
+
+    }
 }
--- src/Model/Searcher.java
+++ src/Model/Searcher.java
@@ -3,10 +3,7 @@ package Model;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -14,32 +11,34 @@ public class Searcher {
 
     Parse parser;
     Ranker ranker;
-    HashMap<String,HashMap<String, Integer>> docsResults;
+    HashMap<String, HashMap<String, Integer>> docsResults;
+    private HashMap<String, Integer> docsInfo;
 
     public Searcher() {
+        docsInfo = new HashMap<>();
     }
 
-    public void search(Indexer indexer, String query, boolean withStemming, String saveInPath, String corpusPath, String queryId, String queryDescription){
+    public void search(Indexer indexer, String query, boolean withStemming, String saveInPath, String corpusPath, String queryId, String queryDescription) {
         docsResults = new HashMap<>();
-        parser = new Parse(withStemming,saveInPath,corpusPath);
+        parser = new Parse(withStemming, saveInPath, corpusPath);
         ranker = new Ranker();
-        HashMap<String,Integer> queryTerms = parser.parseQuery(query);
-        HashMap <String, ArrayList<Integer>> dictionary = indexer.getDictionary();
+        HashMap<String, Integer> queryTerms = parser.parseQuery(query);
+        HashMap<String, ArrayList<Integer>> dictionary = indexer.getDictionary();
+        setDocsInfo(saveInPath + "\\docsInformation.txt");
 
         String postingDir;
-        if (!withStemming){
+        if (!withStemming) {
             postingDir = "\\indexResults\\postingFiles";
-        }
-        else{
+        } else {
             postingDir = "\\indexResults\\postingFiles_Stemming";
         }
         int pointer = 0;
-        for (String term: queryTerms.keySet()) {
-            if(!dictionary.containsKey(term)){
+        for (String term : queryTerms.keySet()) {
+            if (!dictionary.containsKey(term)) {
                 continue;
             }
             pointer = dictionary.get(term).get(2);
-           // char chunk = indexer.classifyToPosting(term).charAt(0);
+            // char chunk = indexer.classifyToPosting(term).charAt(0);
             String chunk = ("" + term.charAt(0)).toUpperCase();
 
             //get the relevant line from posting file
@@ -49,7 +48,7 @@ public class Searcher {
                 String line = "";
                 int i = 1;
                 while ((line = (br.readLine())) != null) {
-                    if(i == pointer){
+                    if (i == pointer) {
                         break;
                     }
                     i++;
@@ -64,15 +63,15 @@ public class Searcher {
             }
         }
 
-       ranker.rank(docsResults, queryTerms,dictionary, queryId, queryDescription);
+        ranker.rank(docsResults, queryTerms, dictionary, docsInfo, queryId, queryDescription);
 
     }
 
     private void findDocsFromLine(String line, String term) {
-        String docs = line.substring(0,line.indexOf("[") - 1); //get 'docsListStr'
+        String docs = line.substring(0, line.indexOf("[") - 1); //get 'docsListStr'
         String[] docsArr = docs.split("; ");
         for (String docInfo : docsArr) {
-            String doc = docInfo.substring(0,docInfo.indexOf(": "));
+            String doc = docInfo.substring(0, docInfo.indexOf(": "));
             String tf = docInfo.substring(docInfo.indexOf(":") + 2);
 /*            if(!docsResults.containsKey(doc)){
                 docsResults.put(doc,term + " -" + tf);
@@ -85,36 +84,52 @@ public class Searcher {
                 HashMap<String, Integer> termsTf = new HashMap<>();
                 termsTf.put(term, Integer.parseInt(tf));
                 docsResults.put(doc, termsTf);
-            }
-            else {
+            } else {
                 HashMap<String, Integer> termsTf = docsResults.get(doc);
                 termsTf.put(term, Integer.parseInt(tf));
                 docsResults.replace(doc, termsTf);
             }
-          //  System.out.println("DocNo: "+ doc + " term&tf: " + docsResults.get(doc));
-        //    System.out.println(doc + " " + docsResults.get(doc));
+            //  System.out.println("DocNo: "+ doc + " term&tf: " + docsResults.get(doc));
+            //    System.out.println(doc + " " + docsResults.get(doc));
         }
     }
 
 
-    public void separateFileToQueries(Indexer indexer, File queriesFile, boolean withStemming, String saveInPath, String corpusPath){
+    public void separateFileToQueries(Indexer indexer, File queriesFile, boolean withStemming, String saveInPath, String corpusPath) {
         try {
-            org.jsoup.nodes.Document document = Jsoup.parse(queriesFile,"UTF-8");
+            org.jsoup.nodes.Document document = Jsoup.parse(queriesFile, "UTF-8");
             org.jsoup.select.Elements elements = document.getElementsByTag("top");
-            for (Element e: elements) {
+            for (Element e : elements) {
                 String queryText = e.select("title").text();
                 String queryId = e.select("num").text();
                 String queryDescription = e.select("desc").text();
-                search(indexer,queryText, withStemming, saveInPath, corpusPath, queryId, queryDescription);
+                search(indexer, queryText, withStemming, saveInPath, corpusPath, queryId, queryDescription);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    public void setDocsInfo(String path) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(new File(path)));
+            String line = "";
+            while ((line = (br.readLine())) != null) {
+                String [] splitLineByDelimiter = line.split(", ");
+                String docId = splitLineByDelimiter[0].substring(0, splitLineByDelimiter[0].indexOf(":"));
+                String docLength = splitLineByDelimiter[1];
+                docsInfo.put(docId, Integer.parseInt(docLength));
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    public static void main(String[] args){
-        Searcher searcher = new Searcher();
-        searcher.findDocsFromLine("FBIS3-21309: 1; LA030290-0012: 2;[10.41]", "invariables");
+
+//    public static void main(String[] args){
+//        Searcher searcher = new Searcher();
+//        searcher.findDocsFromLine("FBIS3-21309: 1; LA030290-0012: 2;[10.41]", "invariables");
+//    }
     }
 }
