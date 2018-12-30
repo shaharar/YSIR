859083183f33444f241b9e2a736bb0914d8b05a2
haliliya <haliliya@post.bgu.ac.il>
2018-12-30 22:04:55 +0200

    bm-25 parameters

--- src/Model/Ranker.java
+++ src/Model/Ranker.java
@@ -25,8 +25,8 @@ public class Ranker {
             }
         });
         queryResults = new HashMap<>();
-        b = 0.75;
-        k = 1.6;
+        b = 0.3;
+        k = 1.2;
     }
     public void rank(HashMap<String, HashMap<String, Integer>> docsResults, HashSet<String> docsOfChosenCities, HashMap<String, Integer> queryTerms, HashMap<String, ArrayList<Integer>> dictionary, HashMap<String, Integer> docsInfo, HashMap <String, Double> weightsPerDoc, String queryId, String queryDescription, String saveInPath) {
         int totalDocsLengths = 0, N;
